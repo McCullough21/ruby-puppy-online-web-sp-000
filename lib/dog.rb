f1d55e227f90cd8e3111633c92083e1f2074e296
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-01 16:03:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,19 +3,13 @@ class Dog
 
   @@all = []
 
+attr_accessor :name
 
   def initialize(name)
     @name = name
     @@all << @name
   end
 # binding.pry
-def name=(name)
-  @name
-end
-
-def name
-  @name
-end
   def self.all
     @@all
   end
