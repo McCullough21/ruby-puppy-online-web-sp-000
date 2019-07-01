e6f9f6a88bdb8fb35b4b4878e8e8202948685ec2
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-01 15:46:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,15 +2,15 @@ class Dog
 
   @@all = []
 
- attr_accessor :name
-
-  def intialize(name)
-    @name = name
-    @@all << name
-  end
-
-  def self.all
-    @@all
-  end
+ # attr_accessor :name
+ #
+ #  def intialize(name)
+ #    @name = name
+ #    @@all << name
+ #  end
+ #
+ #  def self.all
+ #    @@all
+ #  end
 
 end
